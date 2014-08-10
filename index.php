334af06a3089241b515a84433cbb357939c08cb3
Tuan Duong <bacduong@gmail.com>
2014-08-10 23:18:02 +0700

    Remove unused code

--- index.php
+++ index.php
@@ -12,22 +12,3 @@ $input = "8378464";
 $t9input->addDictionary("dictionary.txt", str_split($t9input->key[$input[0]]));
 $result = $t9input->translate($input);
 var_dump($result);
-
-die();
-$trie = new Trie();
-$words = array(
-    'add',
-    'adder',
-    'addled',
-    'alert',
-    'busy',
-    'agree',
-    'agreement',
-    'astronaut'
-);
-
-foreach ($words as $word) {
-    $trie->add($word);
-}
-
-var_dump($trie->prefixSearch('ad'));
